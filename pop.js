ade1725361d2e323a579808bfa7eef8e4a25d485
Rajdip Ghosh <pradipg000@gmail.com>
2024-05-11 19:01:49 +0530

    done

--- pop.js
+++ pop.js
@@ -1,6 +1,20 @@
 
 window.onload = () => {
-    chrome.tabs.query({currentWindow:true,active:true},function(tabs){
+    var customElement = $("<div>", {
+        // "css"   : {
+        //     "border"        : "4px dashed gold",
+        //     "font-size"     : "40px",
+        //     "text-align"    : "center",
+        //     "padding"       : "10px"
+        // },
+       
+    });
+    $.LoadingOverlay("show");
+    setTimeout(() => {
+        $.LoadingOverlay("Hide");
+       
+    }, 2000);
+        chrome.tabs.query({currentWindow:true,active:true},function(tabs){
         var typeNumber = 0;
         var errorCorrectionLevel = 'L';
         var qr = qrcode(typeNumber, errorCorrectionLevel);
@@ -9,7 +23,8 @@ window.onload = () => {
         var holder=document.getElementById('placeHolder')
         var imgurl=qr.createDataURL();
         document.getElementById("download").href=imgurl;
-        holder.innerHTML = qr.createImgTag(3);
+        holder.innerHTML= qr.createImgTag(3);
+        
         document.getElementById('title').innerHTML=tabs[0].title;
 
 
